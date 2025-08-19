89015e30defbace20b13e12357cbf2a0d24bee84
Kondoju Dinesh <169022340+kondojudinesh@users.noreply.github.com>
2025-08-19 22:24:14 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -6,24 +5,34 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import CreateGig from "./pages/CreateGig";
 import Gigs from "./pages/Gigs";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+
+// ✅ added
 import { Toaster } from "react-hot-toast";
 
-const App: React.FC = () => {
+function App() {
   return (
     <Router>
-      {/* Toast notification system */}
-      <Toaster position="top-right" reverseOrder={false} />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/create-gig" element={<CreateGig />} />
+            <Route path="/gigs" element={<Gigs />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create-gig" element={<CreateGig />} />
-        <Route path="/gigs" element={<Gigs />} />
-      </Routes>
+      {/* ✅ Toast notifications container */}
+      <Toaster position="top-right" reverseOrder={false} />
     </Router>
   );
-};
+}
 
 export default App;
